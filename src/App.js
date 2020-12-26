40f18ed0e8696eef7a1f4c0146445589da0eb0e9
huyphung1602 <huy.phung.sw@gmail.com>
2020-12-26 20:17:44 +0700

    Add all the codes.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './TicTacToe.css';
+import Game from './Game';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Tic Tac Toe</h1>
+      </header>
+      <Game />
+    </div>
+  );
+}
+
+export default App;
